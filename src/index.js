d11c4207d364ca6e6da5ceefe9e8c6db42ee8cf2
choi8803 <choijh225@naver.com>
2022-03-15 13:32:26 +0900

    #1.0 Add Hook

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const App = () => {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+
+  return (
+    <div className="App">
+      <h1>Function Component ({item})</h1>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>decrement</button>
+    </div>
+  );
+};
+
+class AppUgly extends React.Component {
+  state = {
+    item: 1
+  };
+
+  render() {
+    const { item } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Class Component ({item})</h1>
+        <button onClick={this.incrementItem}>Increment</button>
+        <button onClick={this.decrementItem}>decrement</button>
+      </div>
+    );
+  }
+  incrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item + 1
+      };
+    });
+  };
+  decrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item - 1
+      };
+    });
+  };
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <AppUgly/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
