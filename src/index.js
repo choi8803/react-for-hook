a5f9796d7a87c39cdf2ab4e3e813eeec51b6119f
choi8803 <choijh225@naver.com>
2022-04-04 17:52:34 +0900

    Add useFadeIn, useNetwork

--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
 import React, {useState, useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
 
-const useBeforeLeave = (onBefore) => {
-  const handle = (event) => {
-    const { clientY } = event;
-    if(clientY <= 0){
-      onBefore();
-    }
-  };
-  useEffect(() => {
-    if(typeof onBefore !== "function"){
-      return;
-    }
-    document.addEventListener("mouseleave", handle);
-    return () => document.removeEventListener("mouseleave", handle);
-  }, []);
-};
 
 const App = () => {
-  const begForLife = () => console.log("Pls dont leave");
-  useBeforeLeave(begForLife);
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>"Hello"</h1>
+      <p>"welcome to Crossfit"</p>
     </div>
   );
 };
