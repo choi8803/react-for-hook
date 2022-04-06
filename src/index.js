cb668231a1fb2516a5e2396b87433a6623fa6c69
choi8803 <choijh225@naver.com>
2022-04-06 16:08:24 +0900

    Add Image

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React, {useState, useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
 
-
 const App = () => {
   return (
-    <div className="App">
-      <h1>"Hello"</h1>
-      <p>"welcome to Crossfit"</p>
+    <div className="App" style={{height:"1000vh"}}>
+      <img src='https://static7.depositphotos.com/1001651/762/i/950/depositphotos_7620905-stock-photo-big-and-small-apples-on.jpg' alt="IU" />
+      <button>Make fullscreen</button>
     </div>
   );
 };
